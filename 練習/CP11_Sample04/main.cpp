26fb95bfb01a58ebda87501909b4eb31c44b639d
unknown <yuzu00117.ponzoo@gmail.com>
2023-11-13 19:30:05 +0900

    11/13 commit

---
+++ 練習/CP11_Sample04/main.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char file_name[40];
+    FILE *file_p;
+
+    printf("ファイル名を入力\n");
+    scanf("%s", file_name);
+
+    // ファイル読み込み設定でオープンする
+    // オープン成功→file_pにアドレスが入る
+    // オープン失敗（ファイルが存在しない）→file_pにNULLが入る
+    file_p = fopen(file_name, "r+");
+    if (file_p == NULL)
+    {
+        printf("ファイルのオープンに失敗");
+        exit(0);
+    }
+
+    // ファイルをクローズ
+    if (fclose(file_p) == EOF)
+    {
+        printf("ファイルのクローズに失敗");
+        exit(0);
+    }
+    return 0;
+}
--- 課題/CP11_kadai04/main.cpp
+++ 課題/CP11_kadai04/main.cpp
@@ -1,3 +1,62 @@
+/*
+
+
+//-------------------------------------------------------------------------------------
+// #name CP11 ルイーダの酒場
+// #description キャラクターを格納する構造体のメモリを動的確保する
+// #make 2023/10/20
+//-------------------------------------------------------------------------------------
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// キャラクターデータ
+struct CHARACTER_DATA
+{
+	char Name[10]; // 名前
+	int HP;			// ヘルス
+	int AD;			// 攻撃力
+};
+
+// 関数宣言
+//void ViewList(CHARACTER_DATA* pCharaData);			// キャラクターリスト表示
+//void CharacterView(CHARACTER_DATA* pCharaData);		// キャラクター閲覧
+//void CharacterCreate(CHARACTER_DATA* pCharaData);	// キャラクター作成
+//void CharacterDelete(CHARACTER_DATA* pCharaData);	// キャラクター削除
+
+int main()
+{
+	CHARACTER_DATA *p;
+	// キャラクターの保存は10人まで
+	p = (CHARACTER_DATA*)malloc(sizeof(CHARACTER_DATA)*10);
+
+	CHARACTER_DATA CharaData[10] = {};
+
+	// TestCharacter 1
+	strcpy(CharaData[0].Name, "Chara1");
+	CharaData[0].HP = 100;
+	CharaData[0].AD = 50;
+
+	// TestCharacter 2
+	strcpy(CharaData[1].Name, "Chara2");
+	CharaData[1].HP = 200;
+	CharaData[1].AD = 100;
+
+	// TestCharacter 6
+	strcpy(CharaData[5].Name, "Chara6");
+	CharaData[5].HP = 500;
+	CharaData[5].AD = 250;
+
+	printf("---------------------------\n");
+	printf("ギルガメッシュの酒場\n");
+	printf("---------------------------\n");
+
+	// メモリ解放
+	free(p);
+	return 0;
+}
+*/
 //-------------------------------------------------------------------------------------
 // #name CP11 ギルガメッシュの酒場
 // #description キャラクターの閲覧と追加、削除ができるプログラム。最低3つの要素を管理すること
@@ -18,15 +77,18 @@ struct CHARACTER_DATA
 
 // 関数宣言
 void ViewList(CHARACTER_DATA* pCharaData);			// キャラクターリスト表示
-void CharacterView(CHARACTER_DATA *pCharaData);		// キャラクター閲覧
+void CharacterView(CHARACTER_DATA* pCharaData);		// キャラクター閲覧
 void CharacterCreate(CHARACTER_DATA* pCharaData);	// キャラクター作成
 void CharacterDelete(CHARACTER_DATA* pCharaData);	// キャラクター削除
 
 int main()
 {
-
+	CHARACTER_DATA *p;
 	// キャラクターの保存は10人まで
+	p = (CHARACTER_DATA*)malloc(sizeof(CHARACTER_DATA)*10);
+
 	CHARACTER_DATA CharaData[10] = {};
+
 	// TestCharacter 1
 	strcpy(CharaData[0].Name, "Chara1");
 	CharaData[0].HP = 100;
@@ -68,7 +130,7 @@ int main()
 			rewind(stdin);
 			break;
 		case 3:
-		CharacterDelete(CharaData);
+			CharacterDelete(CharaData);
 			rewind(stdin);
 			break;
 		case 4:
@@ -94,7 +156,6 @@ void ViewList(CHARACTER_DATA* pCharaData)
 	for (int i = 0; i < 10; i++)
 	{
 		char temp_str[256] = "";
-
 		// 登録ナンバーの表示
 		sprintf(temp_str, "%2d, ", i + 1);
 		// 名前の表示
@@ -106,7 +167,7 @@ void ViewList(CHARACTER_DATA* pCharaData)
 
 //-------------------------------------------------------------------------------------
 // キャラクターデータの閲覧
-void CharacterView(CHARACTER_DATA *pCharaData)
+void CharacterView(CHARACTER_DATA* pCharaData)
 {
 	// キャラクター閲覧
 	ViewList(pCharaData);
